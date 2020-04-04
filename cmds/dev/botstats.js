4dd5d84d417ef099cf4c137452c9d9fb08843afb
mrlivixx <nikidimich@ya.ru>
2020-04-04 16:23:08 +0300

    Update botstats.js

---
+++ cmds/dev/botstats.js
@@ -0,0 +1,32 @@
+const { version } = require("discord.js");
+const moment = require("moment");
+require("moment-duration-format");
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+    try {
+        const duration = moment.duration(bot.uptime).format(" D [дней], H [часа(ов)], m [минут], s [секунд]");
+        const embed = new Discord.RichEmbed()
+            .setAuthor("Показатели бота")
+            .setColor("#a7f442")
+            .setThumbnail('https://discordemoji.com/assets/emoji/3619_discord_online.png')
+            .setTimestamp()
+            .addField("**⭕ | Использование памяти**", `**${(process.memoryUsage().heapUsed / (1000 * 1000)).toFixed(2)} MB**`, true)
+            .addField("**🕑 | Uptime**", `**${duration}**`, true)
+            .addField("**👥 | Пользователей**", `**${bot.users.size.toLocaleString()}**`, true)
+            .addField("**⚙ | Кол-во команд**", `**${bot.commands.size.toLocaleString()}**`, true)
+            .addField("**💡 | Discord.js**", `**v${version}**`, true)
+            .addField("** <:node:695714676151156827> | node.js**", `**${process.version()}**`, true)
+        message.channel.send(embed);
+    } catch (err) {
+        console.log(err);
+    }
+}
+module.exports.help = {
+    name: 'botstats',
+    aliases: ['bs', 'статистика', 'ботстат'],
+    description: 'Статистика бота',
+    usages: { 'f.botstats': `Покажет статистику бота` },
+    category: 'Разработка'
+}
