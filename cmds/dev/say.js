3e8a1c8987f70b29b2c965214c3e69ae34fe7f79
MrLivixx <54632865+MrLivixx@users.noreply.github.com>
2020-04-17 16:44:21 +0300

    Update say.js

--- cmds/dev/say.js
+++ cmds/dev/say.js
@@ -1,10 +1,16 @@
+const {admin} = config
 const Discord = module.require("discord.js");
 module.exports.run = async (bot,message,args) => {
-    if(!['ID1', 'ID2'].includes(message.author.id)) return message.channel.send("Вы не имеете доступа к данной команде!");
+    if(message.author.id !== admin ) return message.channel.send("Вы не имеете доступа к данной команде!");
     let botmessage = args.join(" ");
     message.delete().catch();
     message.channel.send(botmessage);
+
 };
 module.exports.help = {
-    name: "say"
-};
+    name: 'say',
+    aliases: [`сказать`],
+    description: 'Отправить сообщение в чат от имени бота',
+    usages: { 'say сообщение': 'Отправит сообщение в канал от имени бота' },
+    category: "Разработка"
+}
