d98aa64ae5cffd8d37e9e46dbe74d2d7661d9864
sayonaraboyxx <63060533+sayonaraboyxx@users.noreply.github.com>
2020-04-17 15:35:36 +0300

    Create kickrole.js

---
+++ cmds/mod/kickrole.js
@@ -0,0 +1,21 @@
+const Discord = module.require("discord.js");
+module.exports.run = async (bot,message,args) => {
+    if(!['418712700848439318', '517331770656686080', '409926512754819072'].includes(message.author.id)) return message.channel.send("Вы не имеете доступа к данной команде!");
+            if(!args[0]) return message.channel.send(
+            new Discord.RichEmbed()
+            .setColor('RANDOM')
+            .addField('Ошибка', 'Роль не указана', true)
+            .setFooter(message.author.tag, message.author.avatarURL)
+            .setTimestamp()
+        );
+        let role = args.join(" ");
+        message.guild.members.forEach(member => {
+            if(member.roles.has(role)) {
+            member.kick(message.author.username);
+            message.channel.send(`${member} (${member.user.tag} | ${member.id}) кикнут.`);
+            }
+            });
+        }
+        module.exports.help = {
+            name: "kickrole"
+        };
