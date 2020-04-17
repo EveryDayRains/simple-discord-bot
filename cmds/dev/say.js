624293d83adcf3f7f4e47e65a104e6f2c192f778
Vlad Ciphersky <senioreighteen@yandex.ru>
2020-04-17 19:00:33 +0500

    shitcode by sayonara :-1:

--- cmds/dev/say.js
+++ cmds/dev/say.js
@@ -1,16 +1,17 @@
-const {admin} = config
-const Discord = module.require("discord.js");
-module.exports.run = async (bot,message,args) => {
-    if(message.author.id !== admin ) return message.channel.send("Вы не имеете доступа к данной команде!");
-    let botmessage = args.join(" ");
-    message.delete().catch();
-    message.channel.send(botmessage);
+const Discord = require("discord.js");
+module.exports.run = async (bot, message, args) => {
+    if(message.author.id !== config.admin) return message.channel.send(":x: Вы не имеете доступа к данной команде.");
+    const toSay = args.join(" ");
+    if(!toSay) return message.channel.send(":x: | Не указан текст для отправления.").then((m) => { message.delete(5000); m.delete(5000); });
+    
+    if(message.deletable) message.delete();
+    return message.channel.send(toSay);
+}
 
-};
 module.exports.help = {
     name: 'say',
     aliases: [`сказать`],
     description: 'Отправить сообщение в чат от имени бота',
-    usages: { 'say сообщение': 'Отправит сообщение в канал от имени бота' },
+    usages: { 'say <сообщение>': 'Отправит сообщение в канал от имени бота' },
     category: "Разработка"
-}
+};
