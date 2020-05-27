9235289a57f03df769246434af03577f26ee9344
MrLivixx <nikidimich@ya.ru>
2020-05-27 11:11:35 +0300

    Update message.js

--- src/events/message.js
+++ src/events/message.js
@@ -11,14 +11,7 @@ if(message.channel.type == "dm") return;
     return
  }
 
-// let messageArray = message.content.split(" ");
-// let command = messageArray[0].toLowerCase();
-// let args = messageArray.slice(1);
-// bot.command = command;
-// if (!message.content.startsWith(prefix)) return;
-// let cmd = bot.commands.get(command.slice(prefix.length)) || bot.commands.get(bot.aliases.get(command.slice(prefix.length)));
-// if (cmd) cmd.run(bot, message, args);
-// }
+
 if(message.content.startsWith(prefix)) {
 let messageArray = message.content.split(" ");
 let command = messageArray[0].toLowerCase();
