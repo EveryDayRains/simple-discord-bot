36fd234c78603498559568bbd48a99fadf8954fd
Alexander Herasymov <drgerasimova8@gmail.com>
2020-06-06 23:52:03 +0300

    Messed up again

--- src/events/guildMemberRemove.js
+++ src/events/guildMemberRemove.js
@@ -1,9 +1,9 @@
 const { members } = config
 
 module.exports = (client, member) => {
-  const users = client.channels.get(members);
+  const users = client.channels.cache.get(members);
   if (users) {
       if (!member.guild.me.hasPermission('MANAGE_ROLES_OR_PERMISSIONS')) return;
-      users.setName(`🤹 Кол-во юзеров: ${member.guild.members.filter(m => !m.user.bot).size}`)
+      users.setName(`🤹 Кол-во юзеров: ${member.guild.members.cache.filter(m => !m.user.bot).size}`)
   }
 }
