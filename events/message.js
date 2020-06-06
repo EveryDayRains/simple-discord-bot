121f529fae87cd0677a18748dfc4ef032e50a3c4
Alexander Herasymov <drgerasimova8@gmail.com>
2020-06-06 23:24:27 +0300

    Beautifying code ✨

--- events/message.js
+++ events/message.js
@@ -1,19 +1,18 @@
-const {prefix, ideas} = config.prefix
+const { prefix, ideas } = config.prefix
+
 module.exports = async (bot, message) => {
-if(message.author.bot) return;
-if(message.channel.type == "dm") return;
+ if(message.author.bot) return;
+ if(message.channel.type == "dm") return;
 
-
- if (message.channel.id === ideas) {
+ if (message.channel.id == ideas) {
      message.react('👍')
-    await message.react('👎')
+     await message.react('👎')
  } else return
 
-let messageArray = message.content.split(" ");
-let command = messageArray[0].toLowerCase();
-let args = messageArray.slice(1);
-bot.command = command;
-if (!message.content.startsWith(prefix)) return;
-let cmd = bot.commands.get(command.slice(prefix.length)) || bot.commands.get(bot.aliases.get(command.slice(prefix.length)));
-if (cmd) cmd.run(bot, message, args);
+ let messageArray = message.content.split(" ");
+ let command = messageArray[0].toLowerCase();
+ let args = messageArray.slice(1);
+ if (!message.content.startsWith(prefix)) return;
+ let cmd = bot.commands.get(command.slice(prefix.length)) || bot.commands.get(bot.aliases.get(command.slice(prefix.length)));
+ if (cmd) cmd.run(bot, message, args);
 }
