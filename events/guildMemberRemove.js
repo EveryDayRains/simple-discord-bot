ed308415ea94672eac483fe3b30287d1cbdd7e4a
Alexander Herasymov <drgerasimova8@gmail.com>
2020-06-06 23:20:45 +0300

    Beautifying code ✨

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -1,10 +1,9 @@
+const { members } = config
 
-const {members} = config
 module.exports = (client, member) => {
-  let users = client.channels.get(members);
-if (users) {
-    users.setName(`🤹 Кол-во юзеров: ${member.guild.members.filter(m => !m.user.bot).size}`)
-         if (!member.guild.me.hasPermission('MANAGE_ROLES_OR_PERMISSIONS')) return;
-
-}
+  const users = client.channels.get(members);
+  if (users) {
+      if (!member.guild.me.hasPermission('MANAGE_ROLES_OR_PERMISSIONS')) return;
+      users.setName(`🤹 Кол-во юзеров: ${member.guild.members.filter(m => !m.user.bot).size}`)
+  }
 }
