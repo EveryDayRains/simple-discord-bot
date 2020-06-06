cd2304fbd94cd0a3e3c14762ed31b56f82e5b209
Alexander Herasymov <drgerasimova8@gmail.com>
2020-06-06 23:19:12 +0300

    Beautifying code ✨

--- events/ready.js
+++ events/ready.js
@@ -1,27 +1,19 @@
-const {onlineid, guildid} = require ("../config.json")
-module.exports = async (client) => {
+const { onlineid, guildid } = require ("../config.json")
 
+module.exports = async (client) => {
     client.user.setStatus("online"); 
-    setInterval(function(){
+    setInterval (() => {
         switch(Math.ceil(Math.random()*3+1)){ 
           case 1:
-          client.user.setActivity(`на ${client.users.size} участника`,{ type: 'WATCHING'})
-          break;
-          case 2:
-          client.user.setActivity(`на луну`,{ type: 'WATCHING'})
-          break;
-          case 3:
-          client.user.setActivity(`на который час`,{ type: 'WATCHING'})
-          break;
-    }
-      },10 * 1000);
-          
+          client.user.setActivity(`на ${client.users.size} участника`, { type: 'WATCHING'}); break;
+          case 2: client.user.setActivity(`на луну`, { type: 'WATCHING'}); break;
+          case 3: client.user.setActivity(`который час`, { type: 'WATCHING'}); break;
+        }
+    }, 10 * 1000);
     
-        console.log(`Бот работает под ником: ${client.user.tag}!`);
-
-       async function test1() {
+    console.log(`Бот работает под ником: ${client.user.tag}!`);
+    async function test1() {
        client.channels.find(c => c.id === onlineid).setName(`В сети: ${client.guilds.get(guildid).presences.size}`);
-       }; setInterval(test1, 30000)
-        
-    
-    }
+    }; 
+    setInterval(test1, 30000)   
+}
